3c22916c3198c89908a5a6cbf6eeef1c0d2e2175
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2019-12-29 14:26:24 +0800

    Add files via upload

---
+++ view/ambil_otp.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>@TeamONTeaming</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+<body>
+<div id="preloader">
+        <div class="loader"></div>
+    </div>
+
+	<div class="container-contact2">
+		<div class="contact2-map" id="google_map" data-map-x="40.722047" data-map-y="-73.986422" data-pin="images/icons/map-marker.png" data-scrollwhell="0" data-draggable="1"></div>
+
+		<div class="wrap-contact2">
+			<form class="contact2-form validate-form" method="post" action="">
+				<div class="login-form-head">
+                        <span class="contact2-form-title">
+						TEMBAK PAKET TSEL
+					</span>
+
+				<div class="wrap-input2 validate-input" data-validate = "Nomor harus di isi !!">
+					<input class="input2" type="text" name="nomor" placeholder="Awalan 628xxxx">
+					<span class="focus-input2"></span>
+				</div>
+				<div class="wrap-input2 validate-input" data-validate = "Masukkan Key">
+					<input class="input2" type="text" name="key" placeholder="Masukkan Key">
+					<span class="focus-input2"></span>
+				</div>
+				
+				<div class="container-contact2-form-btn">
+						<div class="wrap-contact2-form-btn">
+							<div class="contact2-form-bgbtn"></div>
+							<button class="contact2-form-btn">
+								REQUEST OTP
+							</button>
+						</div>
+					</div>
+			</form>
+		</div>
+	</div>
+
+
+
+	<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+
+</body>
+</html>
---
+++ view/beli_paket.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>@TeamONTeaming</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+<body>
+<div id="preloader">
+        <div class="loader"></div>
+    </div>
+
+	<div class="container-contact2">
+		<div class="contact2-map" id="google_map" data-map-x="40.722047" data-map-y="-73.986422" data-pin="images/icons/map-marker.png" data-scrollwhell="0" data-draggable="1"></div>
+
+		<div class="wrap-contact2">
+			<form class="contact2-form validate-form" method="post" action="">
+				<div class="login-form-head">
+                        <span class="contact2-form-title">
+						Pilih Paket
+					</span>
+					
+				<select class="custom-select" name="id" name="pkgid" onchange="if (this.value=='0'){this.form['id'].style.visibility='visible'}else {this.form['id'];" style="width: 100%;">
+    <option selected>PILIH PAKET...</option>
+    <option value="00016038">OMG 5GB 10K V1 30 HARI</option>
+    <option value="00016036">OMG 5GB 10K V2 30 HARI</option>
+    <option value="00013431">PAKET SMS V1 1 HARI</option>
+    <option value="00013430">PAKET SMS V2 1 HARI</option>
+    <option value="00016030">MAXSTREM 10GB 10K 30 HARI</option>
+    <option value="00016199">WEEKEND DEELS 30GB 30K 30 HARI</option>
+    <option value="00020943">FLASH 50GB 50K 7 HARI (CLOSED)</option>
+    <option value="00015186">GIGAMAX 6GB 25K 30 HARI 30 HARI</option>
+    <option value="00009040">INTERNET MALAM 1GB 2K 2 HARI</option>
+    <option value="00009033">INTERNET MALAM 5GB 5K 2 HARI</option>
+    <option value="00007221">MAXSTREM 1GB 10K 7 HARI</option>
+    <option value="00010654">MAXSTREM 1GB 10K 2 HARI</option>
+    <option value="00009382">OMG 1GB 10RP 2HARI</option>
+    <option value="00007228">MAXSTREM 10GB 10K 7 HARI</option>
+    <option value="00007229">MAXSTREM 7GB 7K 3 HARI</option>
+    <option value="00021308">(NEW) MAXSTREM 1GB 10RP 30 HARI</option>
+    <option value="00024245">(NEW) MAXSTREM 1GB 10RP 6 HARI</option>
+    </select>
+				
+				<div class="container-contact2-form-btn">
+						<div class="wrap-contact2-form-btn">
+							<div class="contact2-form-bgbtn"></div>
+							<button class="contact2-form-btn">
+								TEMBAK PAKET 
+							</button>
+						</div>
+					</div>
+			</form>
+		</div>
+	</div>
+
+
+
+	<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+
+</body>
+</html>
